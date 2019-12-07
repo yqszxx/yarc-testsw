bf940dd23d07680291ce75e038661ff5d1aac65a
yqszxx <yqszxx@gmail.com>
2019-12-07 14:00:58 +0800

    Update to work with yarc-bootloader.

--- Makefile
+++ Makefile
@@ -1,39 +1,16 @@
 TOOLCHAIN=/opt/riscv64-unknown-elf-gcc-8.2.0-2019.05.3-x86_64-linux-ubuntu14/bin/riscv64-unknown-elf-
-EMULATOR=/mnt/hgfs/Exchange/go_build_main_go
 
-build/sprog.o: sprog.s
-	$(TOOLCHAIN)as -march=rv32i -mabi=ilp32 -o $@ $<
-
-build/sprog: build/sprog.o link.ld
-	$(TOOLCHAIN)ld -m elf32lriscv -o $@ -T link.ld $<
-
-sdebug: build/sprog
-	$(EMULATOR) $<
-
-srun: build/sprog
-	$(EMULATOR) $< 2>/dev/null
-
-sdump: build/sprog
-	$(TOOLCHAIN)objdump -d $<
-
-build/cprog: cprog.c _start.s link.ld
-	$(TOOLCHAIN)gcc -march=rv32i -mabi=ilp32 -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -o $@ -T link.ld _start.s $<
-
-cdebug: build/cprog
-	$(EMULATOR) $<
-
-crun: build/cprog
-	$(EMULATOR) $< 2>/dev/null
-
-cdump: build/cprog
-	$(EMULATOR) $< d
+build/cprog: cprog.c link.ld
+	$(TOOLCHAIN)gcc -O0 -march=rv32i -mabi=ilp32 -static -mcmodel=medany -fvisibility=hidden -fno-builtin -nostdlib -nostartfiles -o $@ -T link.ld $<
 
 cdasm: build/cprog
 	$(TOOLCHAIN)objdump -d $<
 
-build/cprog.hex: build/cprog
-	$(TOOLCHAIN)objcopy -O ihex $< $@
+build/cprog.srec: build/cprog
+	$(TOOLCHAIN)objcopy -O srec $< $@
 
-chex: build/cprog.hex
-	cat build/cprog.hex
+upload: build/cprog.srec
+	dd if=$< of=/dev/ttyUSB2
 
+clear:
+	rm build/*
--- cprog.c
+++ cprog.c
@@ -1,20 +1,92 @@
-extern volatile int GPIO;
-extern volatile int UART[];
+#define LED 19
 
-void setGPIO(int s) {
-    GPIO = s;
+#define _REG32(p, i) (*(volatile unsigned int *) ((p) + (i)))
+
+#define GPIO_CTRL_ADDR 0x10012000UL
+#define GPIO_INPUT_EN   (0x04)
+#define GPIO_OUTPUT_EN  (0x08)
+#define GPIO_OUTPUT_VAL (0x0C)
+#define GPIO_IOF_EN     (0x38)
+#define GPIO_IOF_SEL    (0x3C)
+#define GPIO_REG(offset) _REG32(GPIO_CTRL_ADDR, offset)
+
+
+#define UART0_CTRL_ADDR 0x10013000UL
+#define UART_REG_TXFIFO 0x00
+#define UART_REG_RXFIFO 0x04
+#define UART_REG_TXCTRL 0x08
+#define UART_REG_RXCTRL 0x0c
+#define UART_REG_DIV    0x18
+#define IOF0_UART0_MASK 0x00030000UL
+#define UART_TXEN 0x1
+#define UART_RXEN 0x1
+#define UART0_REG(offset) _REG32(UART0_CTRL_ADDR, offset)
+
+#define DELAY_FACTOR 7550
+void delay(int ms) {
+    volatile int i;
+    while (ms--) {
+        i = DELAY_FACTOR;
+        while (i--);
+    }
+}
+
+void setLED(int state) {
+    state &= 0x01;
+    GPIO_REG(GPIO_OUTPUT_VAL)  =  (state << LED) ;
+}
+
+void init() {
+    GPIO_REG(GPIO_INPUT_EN)    &= ~(0x1 << LED) ;
+    GPIO_REG(GPIO_OUTPUT_EN)   |=  (0x1 << LED) ;
+    setLED(0);
+
+    GPIO_REG(GPIO_IOF_SEL) &= ~IOF0_UART0_MASK;
+    GPIO_REG(GPIO_IOF_EN) |= IOF0_UART0_MASK;
+    UART0_REG(UART_REG_DIV) = 1000; // 57600bps
+    UART0_REG(UART_REG_TXCTRL) |= UART_TXEN;
+    UART0_REG(UART_REG_RXCTRL) |= UART_RXEN;
+}
+
+char getchar() {
+    unsigned int rxFifo;
+    while (1) {
+        rxFifo = UART0_REG(UART_REG_RXFIFO);
+        if ((rxFifo & 0x80000000) == 0) return rxFifo & 0xFF;
+    }
+}
+
+void putchar(char c) {
+    while (UART0_REG(UART_REG_TXFIFO) & 0x80000000);
+    UART0_REG(UART_REG_TXFIFO) = c;
+}
+
+
+void puts(char *s) {
+    while (*s != '\0') {
+        putchar(*s++);
+    }
+}
+
+int gets(char *s) {
+    int cnt = 0;
+    while (1) {
+        *s = getchar();
+        if (*s == '\n') break;
+        s++;
+        cnt++;
+    }
+    s++;
+    *s = '\0';
+    return cnt;
 }
 
 void main() {
-    int recv;
+    init();
+    int i = 0;
     while (1) {
-        while (!(UART[0] & 0x01)); // wait for data
-        UART[0] = 0;
-        UART[0] = 0;
-        recv = UART[1];
-        setGPIO(recv);
-        UART[2] = '0' + recv;
-        UART[0] = 0x02;
-        while (UART[0] & 0x02); // wait for tx done
+        setLED(i);
+        delay(1000);
+        i = ~i;
     }
 }
